d29473e4136a0812ee30a9a16aa336242c214fa0
m4s0 <neoomy@gmail.com>
2020-03-21 15:23:26 +0100

    add doctrine, elasticsearch and broadway dependencies

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Broadway\Bundle\BroadwayBundle\BroadwayBundle::class => ['all' => true],
 ];
